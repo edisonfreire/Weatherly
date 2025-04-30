0734ad8a6badf197df124d020d07d8c74bd5315c
Edison <edisonfreire14@gmail.com>
2025-04-30 18:59:06 -0400

    Initial commit

---
+++ Weatherly/WeatherlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherlyApp.swift
+//  Weatherly
+//
+//  Created by Edison Freire on 4/30/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
